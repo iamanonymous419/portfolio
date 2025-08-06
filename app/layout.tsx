75cbb357ac4fda6af30aa8e6e43a9ceb6db3b759
Anonymous <anonymous292009@gmail.com>
2025-08-06 10:42:46 +0000

    some changes v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body
         className={cn(
           inter.variable,
-           poppins.variable,
+          poppins.variable,
           'font-sans antialiased'
         )}
       >
