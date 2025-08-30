a6b8cae406777bdb62579c79ca25ad10e9af9426
Anonymous <anonymous292009@gmail.com>
2025-08-30 07:26:31 +0000

    testing v8

--- iac/main/provider.tf
+++ iac/main/provider.tf
@@ -13,12 +13,12 @@ provider "aws" {
 # KUBERNETES PROVIDER CONFIGURATION
 # =============================================================================
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = try(data.aws_eks_cluster.this.endpoint, "")
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "")
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name, "--region", var.region]
+    args        = ["eks", "get-token", "--cluster-name", try(data.aws_eks_cluster.this.name, ""), "--region", var.region]
     command     = "aws"
   }
 }
@@ -28,8 +28,8 @@ provider "kubernetes" {
 # =============================================================================
 provider "helm" {
   kubernetes = {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host                   = try(data.aws_eks_cluster.this.endpoint, "")
+    cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "")
+    token                  = try(data.aws_eks_cluster_auth.this.token, "")
   }
 }
